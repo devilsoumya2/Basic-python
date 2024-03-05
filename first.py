9db4cb0069e84f462cf853f5b10e5795485de887
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-05 06:34:10 -0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,16 @@
+# x=int(input("enter a number:"))
+# y=int(input("enter a number:"))
+# z=x+y
+# print(z)
+# if x>y:
+#     print("x is greater then y",x)
+
+# else:
+#     print("y is greater",y)
+x=90
+print(type(x))
+print(bin(x))
+# import keyword
+# print(keyword.kwlist)
+a=345
+print(oct(a))
