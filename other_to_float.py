9e20f9a1a91b19fa6eea418817bf03f78c2b0225
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-12 02:26:35 -0700

    Add files via upload

---
+++ other_to_float.py
@@ -0,0 +1,63 @@
+#conversion of other data type to float data type
+
+#int to float
+a=float(40)
+print(a)
+
+#complex to float is not possible
+# a=float(23+50j)
+# print(a)
+
+#bool to float
+a=float(True)
+print(a)
+
+#string to float is no possible if it is a number then it is possible
+# a=float("soumya")
+# print(a)
+a=float("90")
+print(a)
+
+#other to complex
+a=complex(200)
+print(a)
+
+a=complex(24.7)
+print(a)
+
+# a=complex("soumya")
+# print(a)
+a=complex(True)
+print(a)
+
+#other to string
+a=bool(100)
+print(a)
+
+a=bool(18.67)
+print(a)
+
+a=bool(10+5j)
+print(a)
+
+a=bool(100)
+print(a)
+
+a=bool("soumya")
+print(a)
+
+
+a=str(20)
+print(a)
+
+a=str(30.8)
+print(a)
+
+a=str(40+9j)
+print(a)
+
+a=str("soumya")
+print(a)
+
+a=str(True)
+print(a)
---
+++ typeconversion.py
@@ -0,0 +1,32 @@
+#convert one data type to another data type
+a=100
+b=3
+c=a/b
+print(c)
+print(type(c)) #implicity conversion
+a=50 
+b=60.2
+print(c)
+# explicity type conversion
+#flot to int-- 
+a=int(55.78)
+print(a)
+
+# complex to int
+#other to convert int in complex is not possible
+# a=int(23+78j)
+# print(a)
+
+#bool to int
+a=int(False)
+print(a)
+
+#string to int
+# string to int is not possible if it is a number then it is possible
+# b=int("hello")
+# print(b)
+a=int("100")
+print(a)
+
+
+
